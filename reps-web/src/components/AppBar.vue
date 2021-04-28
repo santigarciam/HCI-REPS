b52adc4766cc4aff8f1f72832ac54c72512f124a
tom <67750968+tomcerdeira@users.noreply.github.com>
2021-04-28 17:12:34 -0300

    Continuacion con la app-bar

--- reps-web/src/components/AppBar.vue
+++ reps-web/src/components/AppBar.vue
@@ -1,27 +1,30 @@
 <template>
   <nav>
-    <v-toolbar>
+    <v-toolbar color="black">
       <router-link to="/Home">
       <img class="mr-3" :src="require('../assets/reps1.png')" height="40"/>
       </router-link>
       <router-link to="/Home">
       <v-toolbar-title><span class="titulo1 font-weight-light">RE</span><span class="titulo2">PS</span></v-toolbar-title>
       </router-link>
-      <v-spacer></v-spacer>
+<!--      <v-spacer></v-spacer>-->
       <!--   VER SI SE PUEDE HACER UN TEMPLATE   -->
-      <div class="text-center">
+
+      <router-link to="/Home">
+      <div class="text-center pr-2 pl-12">
         <v-btn
             rounded
             color="primary"
             dark
-        >
-          Home
-        </v-btn>
+            class="style"
+        >Home</v-btn>
       </div>
+      </router-link>
       <!--  -------  -->
-      <v-spacer></v-spacer>
+<!--      <v-spacer></v-spacer>-->
       <!--   VER SI SE PUEDE HACER UN TEMPLATE   -->
-      <div class="text-center">
+      <router-link to="/MisRutinas">
+      <div class="text-center pr-2">
         <v-btn
             rounded
             color="primary"
@@ -30,10 +33,12 @@
           Mis Rutinas
         </v-btn>
       </div>
+      </router-link>
       <!--  -------  -->
-      <v-spacer></v-spacer>
+<!--      <v-spacer></v-spacer>-->
       <!--   VER SI SE PUEDE HACER UN TEMPLATE   -->
-      <div class="text-center">
+      <router-link to="/MisEjercicios">
+      <div class="text-center pr-2">
         <v-btn
             rounded
             color="primary"
@@ -42,10 +47,12 @@
           Mis Ejercicios
         </v-btn>
       </div>
+      </router-link>
       <!--  -------  -->
-      <v-spacer></v-spacer>
+<!--      <v-spacer></v-spacer>-->
       <!--   VER SI SE PUEDE HACER UN TEMPLATE   -->
-      <div class="text-center">
+      <router-link to="/Descubrir">
+      <div class="text-center pr-2">
         <v-btn
             rounded
             color="primary"
@@ -54,18 +61,50 @@
           Descubrir
         </v-btn>
       </div>
+        </router-link>
       <!--  -------  -->
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-circle</v-icon>
-      </v-btn>
+<!--   MENU DE PERFIL   -->
+      <div class="text-center">
+        <v-menu offset-y
+            transition="slide-y-transition"
+            bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon
+                color="primary"
+                dark
+                v-bind="attrs"
+                v-on="on"
+            >
+              <v-avatar>
+                <img
+                    src="https://cdn.vuetifyjs.com/images/john.jpg"
+                    alt="John"
+                >
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-btn plain>Mi Perfil</v-btn>
+            </v-list-item>
+          </v-list>
+          <v-list>
+            <v-list-item>
+              <v-btn plain>Cerrar Sesion</v-btn>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+<!--      -->
     </v-toolbar>
     </nav>
 </template>
 
 <script>
 export default {
-  name: "AppBar"
+  name: "AppBar",
 }
 </script>
 
