e3223a41e18f646720da470f4c0ff355b02938c4
Santiago <sangarcia@itba.edu,ar>
2021-05-14 18:04:29 -0300

    Update shareRoutine.vue

--- reps-web/src/components/shareRoutine.vue
+++ reps-web/src/components/shareRoutine.vue
@@ -6,10 +6,14 @@
       color="grey"
       v-model="boton"
       class="mt-4 mr-2"
-      v-on:click="showSnackbar"
+      v-on:click="copyAndShowSnackBar"
+
+      value="HOLA"
+      id="myInput"
   >
     <v-icon class="pb-3">mdi-share</v-icon>
   </v-btn>
+<!--    <input  type="text" value="Hello World" id="myInput">-->
   <v-snackbar
       v-model="snackbar"
   >¡Se ha copiado al clipboard el link de la rutina!</v-snackbar>
@@ -17,25 +21,39 @@
 </template>
 
 <script>
+
+import {router} from "../main";
+
 export default {
   props: ['rutina'],
 
   data(){
     return {
       snackbar: false,
+      testingCode:"1234"
     }
   },
 
+
   methods:{
-    showSnackbar: function (event) {
+    copyAndShowSnackBar: async function (event) {
       event.stopPropagation();
-      console.log(this);
-      // alert("PRUEBA");
+      console.log(router);
+      const a = navigator.appName;
+      console.log(a);
+
+      await navigator.clipboard.writeText(`http://localhost:8083/#/routines/${this.rutina.id}`); //// AVISAR QUE CAMBIEN ESTA URL PARA Q FUNCIONEE
+
+     //  alert("PRUEBA");
       this.snackbar = true;
       setTimeout(() => {
         this.$emit("yourEvent");
       }, this.timeout);
-    }
+    },
+
+
+  },
   }
-}
+
+
 </script>
