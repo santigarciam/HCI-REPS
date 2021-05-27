55544c33875e8bd6039b1e1946b053f76c05c9d0
tom <67750968+tomcerdeira@users.noreply.github.com>
2021-05-27 17:08:09 -0300

    icono en las distintas calidades

--- android/reps-app/app/src/main/java/com/example/reps/MainActivity.java
+++ android/reps-app/app/src/main/java/com/example/reps/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.reps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int value = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickMeFunc(View view) {
+        value++;
+        final TextView mTextView = (TextView) findViewById(R.id.textViewOnClick);
+        mTextView.setText("Value :" + value);
     }
 }
