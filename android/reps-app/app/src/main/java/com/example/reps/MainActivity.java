7ab1a6cb8f72a1190b3d2e7206ca8e7662fa85ce
Santiago <sangarcia@itba.edu,ar>
2021-06-02 12:58:46 -0300

    Navegacion entre login y register

--- android/reps-app/app/src/main/java/com/example/reps/MainActivity.java
+++ android/reps-app/app/src/main/java/com/example/reps/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ///// Para que no se vea el titel bar
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        /////
         setContentView(R.layout.activity_main);
     }
+
 
 
 }
