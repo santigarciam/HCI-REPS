ebcf9900fd5f35cf32d1fc9788ad908367943cce
tom <67750968+tomcerdeira@users.noreply.github.com>
2021-06-02 18:15:00 -0300

    Comienzo de RutineCard

---
+++ android/reps-app/app/src/main/java/com/example/reps/RoutineCardAdapter.java
@@ -0,0 +1,64 @@
+package com.example.reps;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class RoutineCardAdapter extends RecyclerView.Adapter<RoutineCardAdapter.ViewHolder>{
+
+    private List<RoutineCard> routines;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public RoutineCardAdapter(List<RoutineCard> routines, Context context) {
+        this.routines = routines;
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public RoutineCardAdapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.routine_card, null);
+        return new RoutineCardAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RoutineCardAdapter.ViewHolder holder, int position) {
+        holder.bindData(routines.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return routines.size();
+    }
+
+    public void setroutines(List<RoutineCard> rut) { routines = rut; }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name, owner;
+
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.rutine_card_title);
+            owner = itemView.findViewById(R.id.rutine_card_user);
+        }
+
+        void bindData(final RoutineCard item){
+            name.setText(item.getName());
+            owner.setText(item.getOwner());
+        }
+    }
+}
