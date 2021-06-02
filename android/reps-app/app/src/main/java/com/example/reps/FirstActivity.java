fda33ea1588756c2fe53894e0451b2bd2bad9667
tom <67750968+tomcerdeira@users.noreply.github.com>
2021-06-02 12:04:59 -0300

    first, login y register activities

---
+++ android/reps-app/app/src/main/java/com/example/reps/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.example.reps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+}
