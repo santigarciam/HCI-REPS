278842676afb6c71f2e38f2f4e8a62cfc7302630
Santiago <sangarcia@itba.edu,ar>
2021-06-02 16:49:54 -0300

    Comienzo bottom nav bar

---
+++ android/reps-app/app/src/main/java/com/example/reps/ui/perfil/PerfilViewModel.java
@@ -0,0 +1,19 @@
+package com.example.reps.ui.perfil;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PerfilViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public PerfilViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Perfil");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
