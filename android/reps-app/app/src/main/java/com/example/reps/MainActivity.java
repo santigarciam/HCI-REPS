a2ac8007bae20530d3eb494d61786f1e0efbabec
Santiago <sangarcia@itba.edu,ar>
2021-06-08 16:13:15 -0300

    Remplazo de actividad de vista de rutinas a fragmento home

--- android/reps-app/app/src/main/java/com/example/reps/MainActivity.java
+++ android/reps-app/app/src/main/java/com/example/reps/MainActivity.java
@@ -13,14 +13,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    int value = 0;
-    List<RoutineCard> rutinas;
 
-    //////////////////
-    RecyclerView verticalRecyclerView;
-    RoutineSectionAdapter verticalAdapter;
-    RecyclerView horizontalRecyclerView;
-    ArrayList<RoutineSection> arrayList1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,48 +25,11 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         /////
-
-        setContentView(R.layout.activity_home);
-        init();
-
+        setContentView(R.layout.activity_main);
 
     }
 
-    public void init(){
-        rutinas = new ArrayList<>();
-        rutinas.add(new RoutineCard("Rut Tomas", "Paulo"));
-        rutinas.add(new RoutineCard("Rut Lu", "Gaston"));
-        rutinas.add(new RoutineCard("Rut Tomas", "Paulo"));
-        rutinas.add(new RoutineCard("Rut Lu", "Gaston"));
-        rutinas.add(new RoutineCard("Rut Tomas", "Paulo"));
-        rutinas.add(new RoutineCard("Rut Lu", "Gaston"));
-        rutinas.add(new RoutineCard("Rut Tomas", "Paulo"));
-        rutinas.add(new RoutineCard("Rut Lu", "Gaston"));
 
-        RoutineCardAdapter rAdapter = new RoutineCardAdapter(rutinas, this);
-        //RecyclerView recyclerView = findViewById(R.id.section_rout_recycler_view);
-        //recyclerView.setHasFixedSize(true);
-        //LinearLayoutManager lManager = new LinearLayoutManager(this);
-        //lManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        //recyclerView.setLayoutManager(lManager);
-        //recyclerView.setAdapter(rAdapter);
-
-        ////////////////////////////////
-        verticalRecyclerView = findViewById(R.id.section_rout_recycler_view);
-        verticalRecyclerView.setHasFixedSize(true);
-        verticalRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
-
-        arrayList1 = new ArrayList<>();
-        verticalAdapter = new RoutineSectionAdapter(arrayList1,this);
-        verticalRecyclerView.setAdapter(verticalAdapter);
-        RoutineSection verticalModel = new RoutineSection("Prueba 1", rutinas);
-        arrayList1.add(verticalModel);
-        RoutineSection verticalModel2 = new RoutineSection("Prueba 2", rutinas);
-        arrayList1.add(verticalModel2);
-        RoutineSection verticalModel3 = new RoutineSection("Prueba 3", rutinas);
-        arrayList1.add(verticalModel3);
-        verticalAdapter.notifyDataSetChanged();
-    }
 
 
 }
