bcf0d8f0a7422f2ff466557847054f19ce89d0ba
Santiago <sangarcia@itba.edu,ar>
2021-06-08 15:33:53 -0300

    Fragment Perfil comienzo

--- android/reps-app/app/src/main/java/com/example/reps/LogedActivity.java
+++ android/reps-app/app/src/main/java/com/example/reps/LogedActivity.java
@@ -32,6 +32,7 @@ public class LogedActivity extends AppCompatActivity {
         binding = ActivityLogedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
