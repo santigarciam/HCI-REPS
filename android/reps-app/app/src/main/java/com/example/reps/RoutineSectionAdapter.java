10a94d6a0e28f006096246f32da7e837160c74cc
Santiago <sangarcia@itba.edu,ar>
2021-06-09 13:43:37 -0300

    Update RoutineSectionAdapter.java

--- android/reps-app/app/src/main/java/com/example/reps/RoutineSectionAdapter.java
+++ android/reps-app/app/src/main/java/com/example/reps/RoutineSectionAdapter.java
@@ -14,6 +14,7 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.reps.ui.home.HomeFragment;
 import com.example.reps.ui.home.HomeFragmentDirections;
 
 import org.jetbrains.annotations.NotNull;
