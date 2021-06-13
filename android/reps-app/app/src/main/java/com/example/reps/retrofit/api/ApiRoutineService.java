18c1036efe31b6b196356fbd3c8ed5c9d9951f29
Santiago <sangarcia@itba.edu,ar>
2021-06-13 11:12:29 -0300

    Apiroutine y favourite services

---
+++ android/reps-app/app/src/main/java/com/example/reps/retrofit/api/ApiRoutineService.java
@@ -0,0 +1,37 @@
+package com.example.reps.api;
+
+import androidx.lifecycle.LiveData;
+
+
+import com.example.reps.retrofit.api.ApiResponse;
+import com.example.reps.retrofit.api.model.Cycle;
+import com.example.reps.retrofit.api.model.CycleExercise;
+import com.example.reps.retrofit.api.model.PagedList;
+import com.example.reps.retrofit.api.model.Routine;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiRoutineService {
+
+    @GET("routines?{params}")
+    LiveData<ApiResponse<PagedList<Routine>>> getAll(@Path("params") String params);
+
+    @GET("routines/{routineId}")
+    LiveData<ApiResponse<Routine>> getRoutine(@Path("routineId") int routineId);
+
+    //cycles
+    @GET("routines/{routineId}/cycles")
+    LiveData<ApiResponse<PagedList<Cycle>>> getRoutineCycles(@Path("routineId") int routineId);
+
+    @GET("routines/{routineId}/cycles/{cycleId}")
+    LiveData<ApiResponse<Cycle>> getCycle(@Path("routineId") int routineId, @Path("cycleId") int cycleId);
+
+    @GET("cycles/{cycleId}/exercises")
+    LiveData<ApiResponse<PagedList<CycleExercise>>> getCycle(@Path("cycleId") int cycleId);
+
+
+
+
+
+}
