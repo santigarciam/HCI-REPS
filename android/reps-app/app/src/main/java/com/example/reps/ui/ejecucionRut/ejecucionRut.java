aa2baee6ee296e052ce41a3354f52a7df43236ef
Santiago <sangarcia@itba.edu,ar>
2021-06-14 12:38:48 -0300

    Comienzo de ejecucion
    Falta todo pensar como hacerlo

--- android/reps-app/app/src/main/java/com/example/reps/ui/ejecucionRut/ejecucionRut.java
+++ android/reps-app/app/src/main/java/com/example/reps/ui/ejecucionRut/ejecucionRut.java
@@ -16,14 +16,17 @@ import com.example.reps.retrofit.api.model.Cycle;
 import com.example.reps.retrofit.api.model.CycleExercise;
 import com.example.reps.retrofit.api.model.Exercise;
 import com.example.reps.retrofit.api.model.PagedList;
+import com.example.reps.retrofit.api.model.Routine;
 import com.example.reps.retrofit.api.repository.Status;
-
+import com.example.reps.databinding.ActivityEjecucionRutBinding;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class ejecucionRut extends AppCompatActivity {
 
+    private ActivityEjecucionRutBinding binding;
+    private Routine routine;
     private CycleExercise current;
     private CycleExercise next;
     private PagedList<Cycle> routineCycles;
@@ -37,17 +40,25 @@ public class ejecucionRut extends AppCompatActivity {
         super.onCreate(savedInstanceState); //routine
         app = (App) getApplication();
         ejecucionRutArgs args = ejecucionRutArgs.fromBundle(savedInstanceState);
+        binding = ActivityEjecucionRutBinding.inflate(getLayoutInflater());
+
+
         app.getRoutineRepository().getRoutine(args.getIdRut()).observe(this,r->{
             if(r.getStatus() == Status.SUCCESS){
-                Log.d("BORRAR", "onCreate: ");
+                routine = r.getData();
+                app.getRoutineRepository().getRoutineCycles(r.getData().getId()).observe(this,c->{
+                    if(c.getStatus() == Status.SUCCESS){
+                        routineCycles = c.getData();
+                    }
+                });
             }
         });
+
 
         setContentView(R.layout.activity_ejecucion_rut);
 
         TextView timer = findViewById(R.id.ex_time);
         timer.setText("00:00");
-
         TextView ciclo = findViewById(R.id.nombreCiclo);
         ciclo.setText(currentCycle.getName());
         TextView current_ex = findViewById(R.id.nombre_ejercicio);
