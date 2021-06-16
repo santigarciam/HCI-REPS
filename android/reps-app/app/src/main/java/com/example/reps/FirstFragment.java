bbd086373e14edad44af7fde73d022ed8d3e04e7
tom <67750968+tomcerdeira@users.noreply.github.com>
2021-06-16 12:40:17 -0300

    url a rutina (no loggeado)

--- android/reps-app/app/src/main/java/com/example/reps/FirstFragment.java
+++ android/reps-app/app/src/main/java/com/example/reps/FirstFragment.java
@@ -1,7 +1,9 @@
 package com.example.reps;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,6 +63,7 @@ public class FirstFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
